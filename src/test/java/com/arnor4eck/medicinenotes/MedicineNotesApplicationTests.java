db5f639d04f52e7a20b051e66d9138f165af5c48
Nikita Kivalin <kivalinnik@mail.ru>
2026-01-15 23:51:34 +0300

    init commit

---
+++ src/test/java/com/arnor4eck/medicinenotes/MedicineNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnor4eck.medicinenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
