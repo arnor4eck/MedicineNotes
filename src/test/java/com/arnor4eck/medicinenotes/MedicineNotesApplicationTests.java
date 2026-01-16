ce376727a4cdd791999303cac999ec80d20a82a6
Nikita Kivalin <kivalinnik@mail.ru>
2026-01-17 00:56:02 +0300

    updated main test

--- src/test/java/com/arnor4eck/medicinenotes/MedicineNotesApplicationTests.java
+++ src/test/java/com/arnor4eck/medicinenotes/MedicineNotesApplicationTests.java
@@ -2,8 +2,10 @@ package com.arnor4eck.medicinenotes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 class MedicineNotesApplicationTests {
 
     @Test
