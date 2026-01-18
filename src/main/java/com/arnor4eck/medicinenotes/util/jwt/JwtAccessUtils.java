7a57b128a4d0576d0aff35812bc9dd14d9255db9
Nikita Kivalin <kivalinnik@mail.ru>
2026-01-18 22:31:22 +0300

    updated jwt utils

--- src/main/java/com/arnor4eck/medicinenotes/util/jwt/JwtAccessUtils.java
+++ src/main/java/com/arnor4eck/medicinenotes/util/jwt/JwtAccessUtils.java
@@ -1,16 +1,23 @@
 package com.arnor4eck.medicinenotes.util.jwt;
 
+import com.arnor4eck.medicinenotes.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
-public class JwtAccessUtils implements JwtUtils<String>{
+public class JwtAccessUtils implements JwtUtils<User>{
     @Value("${jwt.secret}")
     private String secret;
 
@@ -18,19 +25,23 @@ public class JwtAccessUtils implements JwtUtils<String>{
     private long lifetime;
 
     @Override
-    public String generateToken(String object) {
+    public String generateToken(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
         Date issued = new Date();
         Date expired = new Date(issued.getTime() + lifetime);
+        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
         return Jwts.builder()
-                .subject(object)
+                .claims(claims) // полезная информация
+                .subject(user.getEmail())
                 .issuedAt(issued) // создание
                 .expiration(expired) // истечение
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8))) // секрет
+                .signWith(key) // секрет
                 .compact();
     }
 
-    @Override
-    public boolean validate(String token) {
+    public boolean validate(String token){
         try {
             Jwts.parser()
                     .verifyWith(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
@@ -40,5 +51,21 @@ public class JwtAccessUtils implements JwtUtils<String>{
         } catch (JwtException | IllegalArgumentException e) {
             return false;
         }
+    }
+
+    public String getEmail(String token){
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public List<String> getRoles(String token){
+        return getClaimsFromToken(token).get("roles", List.class);
+    }
+
+    private Claims getClaimsFromToken(String token){
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
