a39eba88da9aea17db71733e4ceaed5146f9e4c6
Nikita Kivalin <kivalinnik@mail.ru>
2026-01-20 14:24:34 +0300

    add scheduler to set skipped intakes

--- src/main/java/com/arnor4eck/medicinenotes/repository/IntakeRepository.java
+++ src/main/java/com/arnor4eck/medicinenotes/repository/IntakeRepository.java
@@ -1,7 +1,6 @@
 package com.arnor4eck.medicinenotes.repository;
 
 import com.arnor4eck.medicinenotes.entity.Intake;
-import com.arnor4eck.medicinenotes.entity.IntakesStatus;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,11 +10,12 @@ import org.springframework.data.repository.query.Param;
 import java.util.Collection;
 
 public interface IntakeRepository extends JpaRepository<Intake,Long> {
-    @Query(value = "SELECT i.* FROM intakes AS i\n" +
-            "JOIN templates AS t on t.id = i.template_id\n" +
-            "JOIN users AS u on u.id = t.user_id\n" +
-            "WHERE u.email = :email\n" +
-            "ORDER BY adoptedin", nativeQuery = true)
+    @Query(value = """
+            SELECT i.* FROM intakes AS i
+            JOIN templates AS t on t.id = i.template_id
+            JOIN users AS u on u.id = t.user_id
+            WHERE u.email = :email
+            ORDER BY adoptedin""", nativeQuery = true)
     Collection<Intake> findAllByCreatorEmail(@Param("email") String email);
 
     @Modifying
@@ -38,4 +38,11 @@ public interface IntakeRepository extends JpaRepository<Intake,Long> {
             "    WHERE u.email = :email  AND i.id = :id)", nativeQuery = true)
     int setDone(@Param("id") long id,
                 @Param("email") String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE intakes SET status = 'SKIPPED'\n" +
+            "WHERE shouldAdoptedin < (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/Moscow')::date\n" +
+            "AND status = 'PENDING'", nativeQuery = true)
+    int setSkipped();
 }
