1eb459009486a7c1b28cade8b858269a936aec4e
Nikita Kivalin <kivalinnik@mail.ru>
2026-01-21 22:55:29 +0300

    front: added template service

---
+++ frontend/src/service/medicineTemplateService.ts
@@ -0,0 +1,38 @@
+import api from "./api.ts";
+import type {MedicineTemplate} from "../types/types.ts";
+
+const TEMPLATE_URL = "/v1/template"
+
+class MedicineTemplateService {
+    async getAllUserTemplates(): Promise<MedicineTemplate[]> {
+        try {
+            const res = await api.get<MedicineTemplate[]>(TEMPLATE_URL + "/my");
+            return res.data;
+        } catch (error) {
+            console.error('Ошибка получения всех шаблонов: ', error);
+            throw error;
+        }
+    }
+
+    async createMedicineTemplate(request : Omit<MedicineTemplate, 'id'>): Promise<MedicineTemplate> {
+        try {
+            const res = await api.post<MedicineTemplate>(TEMPLATE_URL + "/create", request);
+            return res.data;
+        } catch (error) {
+            console.error('Ошибка создания шаблона: ', error);
+            throw error;
+        }
+    }
+
+    async getMedicineTemplateById(id: number): Promise<MedicineTemplate> {
+        try {
+            const res = await api.get<MedicineTemplate>(TEMPLATE_URL + `/${id}`);
+            return res.data;
+        } catch (error) {
+            console.error(`Ошибка получения шаблона с id  ${id}: `, error);
+            throw error;
+        }
+    }
+}
+
+export const medicineTemplateService = new MedicineTemplateService();
