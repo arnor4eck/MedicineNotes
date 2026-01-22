9c36af8b657ad123bd3dd7357649f0425a056d6e
Nikita Kivalin <kivalinnik@mail.ru>
2026-01-22 15:01:59 +0300

    frontend: added header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import { Link, useLocation } from 'react-router-dom';
+import './Header.css'
+import {authService} from "../../service/authService.ts";
+
+export default function Header() {
+    const location = useLocation();
+
+    const navItems = [
+        { path: '/templates', label: 'Шаблоны' },
+        authService.isAuthenticated() ? { path:  '/logout', label: 'Выйти' }
+            : { path: 'auth', label: 'Авторизоваться' }
+    ];
+
+    return (
+        <div className='center_class'>
+        <header className="main-navigation">
+            <div className="nav-brand">
+                <h2>MedicineNotes</h2>
+            </div>
+
+            <ul className="nav-menu">
+                {navItems.map((item) => (
+                    <li key={item.path}>
+                        <Link
+                            to={item.path}
+                            className={
+                                location.pathname === item.path
+                                    ? 'nav-link active'
+                                    : 'nav-link'
+                            }
+                        >
+                            {item.label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </header>
+        </div>
+    );
+}
