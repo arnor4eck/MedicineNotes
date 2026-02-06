db11a157be56930dfa233158de4843e4e0ab0f29
Nikita Kivalin <kivalinnik@mail.ru>
2026-02-06 12:16:30 +0300

    backend: updated tests

---
+++ backend/src/test/java/com/arnor4eck/medicinenotes/TestConfig.java
@@ -0,0 +1,26 @@
+package com.arnor4eck.medicinenotes;
+
+import com.arnor4eck.medicinenotes.util.controller_advice.ExceptionResponseFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@TestConfiguration
+public class TestConfig {
+    @Bean
+    @Primary
+    public ExceptionResponseFactory exceptionResponseFactory() {
+        ExceptionResponseFactory factory = mock(ExceptionResponseFactory.class);
+
+        when(factory.create(anyString(), any(), any())).thenCallRealMethod();
+        when(factory.create(anyList(), any(), any())).thenCallRealMethod();
+
+        return factory;
+    }
+}
