e44254ccb7fcb24651dae4a3d97d9b96daa648ae
arnor4eck <KivalinNik@mail.ru>
2026-02-19 11:17:45 +0300

    backend: updated statistics service

--- backend/src/main/java/com/arnor4eck/medicinenotes/service/StatisticsService.java
+++ backend/src/main/java/com/arnor4eck/medicinenotes/service/StatisticsService.java
@@ -1,14 +1,17 @@
 package com.arnor4eck.medicinenotes.service;
 
+import com.arnor4eck.medicinenotes.entity.IntakesStatus;
 import com.arnor4eck.medicinenotes.entity.MedicineTemplate;
 import com.arnor4eck.medicinenotes.repository.IntakeRepository;
 import com.arnor4eck.medicinenotes.util.statistics.FullTemplateStatistics;
-import com.arnor4eck.medicinenotes.util.statistics.IntakeStatisticsUnit;
+import com.arnor4eck.medicinenotes.util.statistics.IntakesStatisticsByDate;
+import com.arnor4eck.medicinenotes.util.statistics.IntakesStatisticsByDateUnit;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -18,9 +21,22 @@ public class StatisticsService {
 
     private final MedicineTemplateService templateService;
 
-    public Collection<IntakeStatisticsUnit> getIntakeStatisticsByDateAndEmail(LocalDate date,
+    public IntakesStatisticsByDate getIntakeStatisticsByDateAndEmail(LocalDate date,
                                                                               String email){
-        return intakeRepository.getIntakeStatisticsByDate(date, email);
+        Map<String, IntakesStatisticsByDateUnit> units = new HashMap<>();
+
+        intakeRepository.getIntakeStatisticsByDate(date, email).forEach(el -> {
+            units.compute(el.name(), (k, v) -> {
+                if(v == null)
+                    v = new IntakesStatisticsByDateUnit();
+
+                v.setCountForStatus(IntakesStatus.valueOf(el.status()), el.count());
+
+                return v;
+            });
+        });
+
+        return new IntakesStatisticsByDate(units);
     }
 
     public FullTemplateStatistics getFullTemplateStatisticsByIdAndEmail(long templateId, int offset,
@@ -28,6 +44,7 @@ public class StatisticsService {
 
         MedicineTemplate template = templateService.getTemplateByIdCreator(templateId, email);
 
-        return new FullTemplateStatistics(intakeRepository.getTemplateStatisticsOfDone(templateId, offset), template.getQuantityPerDay());
+        return new FullTemplateStatistics(intakeRepository.getTemplateStatisticsOfDone(templateId, offset),
+                template.getQuantityPerDay());
     }
 }
