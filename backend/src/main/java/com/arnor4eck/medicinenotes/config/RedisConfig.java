b693106199a44451d5fd6b246960007b0bd8dba5
arnor4eck <KivalinNik@mail.ru>
2026-04-16 10:35:30 +0300

    added redis

---
+++ backend/src/main/java/com/arnor4eck/medicinenotes/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.arnor4eck.medicinenotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory);
+        return template;
+    }
+}
+
