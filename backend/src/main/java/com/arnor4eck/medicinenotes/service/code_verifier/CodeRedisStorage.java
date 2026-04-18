d36ff434715f7e68f8da7f21d6cb3aeb632cc689
arnor4eck <KivalinNik@mail.ru>
2026-04-18 21:19:25 +0300

    updated CodeRedisStorage

--- backend/src/main/java/com/arnor4eck/medicinenotes/service/code_verifier/CodeRedisStorage.java
+++ backend/src/main/java/com/arnor4eck/medicinenotes/service/code_verifier/CodeRedisStorage.java
@@ -5,12 +5,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashMap;
+
 /** Класс для хранения отправленного кода подтверждения в Redis
  * */
 @Component
 public class CodeRedisStorage extends AbstractRedisStorage<PreRegistration>{
     public CodeRedisStorage(RedisTemplate<String, Object> redisTemplate,
                             @Value("${redis.timeout.code}") int timeout) {
-        super(redisTemplate, "code:", timeout, o -> (PreRegistration) o);
+        super(redisTemplate, "code:", timeout, o -> {
+            @SuppressWarnings("unckecked") // При десериализации объекта возвращается LinkedHashMap - приведение корректно
+            LinkedHashMap<String, String> map = (LinkedHashMap<String, String>) o;
+            return new PreRegistration(map.get("email"), map.get("username"), map.get("password"), map.get("code"));
+        });
     }
 }
