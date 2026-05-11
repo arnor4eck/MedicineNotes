5baba292d3c3dfc6c32d3dbfc8efae489ff5bcd3
arnor4eck <KivalinNik@mail.ru>
2026-05-11 20:18:46 +0300

    updated templates page

--- frontend/src/components/template/card/TemplateCard.tsx
+++ frontend/src/components/template/card/TemplateCard.tsx
@@ -5,12 +5,13 @@ interface TemplateCardProps {
     id: number;
     name: string;
     until: string;
+    start: string;
 }
 
 export default function TemplateCard(props : TemplateCardProps){
     const navigate = useNavigate();
 
-    const { id, name, until} = props;
+    const { id, name, until, start } = props;
 
     return(
       <article className='template-card'>
@@ -18,6 +19,7 @@ export default function TemplateCard(props : TemplateCardProps){
               <p className='template-card__title'>{name}</p>
           </div>
           <div className='template-card__bottom'>
+              <p className='template-card__deadline'><b>C: {start}</b></p>
               <button onClick={() => navigate(`/templates/${id}`)} className='template-card__button template-card__button--primary'> Подробнее</button>
               <p className='template-card__deadline'><b>До: {until}</b></p>
           </div>
